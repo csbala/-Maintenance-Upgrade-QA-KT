299c9a1878122c69fb02f7b7556c9d936004ea56
Balazs Cservenak <balazs_cservenak@epam.com>
2022-10-25 16:55:34 +0200

    romantoDec and toCamelCase is done

--- jw-webinar-KT/module-4/romanToDec.js
+++ jw-webinar-KT/module-4/romanToDec.js
@@ -7,3 +7,32 @@
  * @returns {number} the Arabic (decimal) equivalent of the parameter
  * @throws Error in case of invalid input
  */
+
+module.exports = function romanToDec(roman) {
+  let validRomans = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
+  let regexp = /^M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$/;
+  let result = 0;
+
+  if (typeof roman != "string") {
+    console.warn("Not a string");
+    return false;
+  }
+  const fixesRoman = roman
+    .split("")
+    .map((char) => char.toUpperCase())
+    .join("");
+
+  if (!regexp.test(fixesRoman) || !fixesRoman) {
+    console.warn("Not a valid roman number");
+    return false;
+  }
+  
+  for (const char of fixesRoman) {
+    if (
+      validRomans[fixesRoman[fixesRoman.indexOf(char) + 1]] > validRomans[char]
+    ) {
+      result -= validRomans[char];
+    } else result += validRomans[char];
+  }
+  return result;
+};
--- jw-webinar-KT/module-4/toCamelCase.js
+++ jw-webinar-KT/module-4/toCamelCase.js
@@ -5,3 +5,25 @@
  * @param {string} toConvert
  * @returns {string} camel-case string or empty string in other cases
  */
+
+const symbols = "abcdefghijklmnopqrstuvwxyz0123456789 ";
+
+module.exports = function toCamelCase(toConvert) {
+  if (typeof toConvert !== "string") return "";
+  return toConvert
+    .split("")
+    .map((char) => {
+      return symbols.includes(char.toLowerCase()) ? char : " ";
+    })
+    .join("")
+    .split(" ")
+    .filter(Boolean)
+    .map((word, index) => {
+      if (!index) {
+        return word.toLowerCase();
+      } else {
+        return word[0].toUpperCase() + word.slice(1).toLowerCase();
+      }
+    })
+    .join("");
+};
